c39a4c2b75d90c50eb1ea70888614a52d96b286d
petarmarinov <peter.marinov@athracian.com>
2019-08-29 15:30:59 +0300

    Adding dropdown menu and dropdown node component

---
+++ Ucommerce.Sitefinity.UI/assets/src/js/components/dropdown-menu.vue
@@ -0,0 +1,32 @@
+﻿<template>
+    <ul class="category-parent">
+        <li v-for="node in nodes">
+            <a :href="node.Url">
+                {{node.DisplayName}}
+                <i v-if="node.Categories.length > 0" class="fa fa-chevron-down"></i>
+            </a>
+
+            <dropdown-node v-if="node.Categories.length > 0" v-for="childNode in node.Categories" v-bind:key="childNode.DisplayName" :node="childNode"></dropdown-node>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import dropdownNode from "../components/dropdown-node";
+    export default {
+        name: "dropdownMenu",
+        props: {
+            nodes: {
+                type: Array,
+                default: []
+            },
+            isOpen: {
+                type: Boolean,
+                default: false
+            }
+        },
+        components: {
+            dropdownNode
+        }
+    };
+</script>
--- Ucommerce.Sitefinity.UI/assets/src/js/widgets/category-navigation.js
+++ Ucommerce.Sitefinity.UI/assets/src/js/widgets/category-navigation.js
@@ -1,11 +1,19 @@
 ﻿import { initializeComponent } from "../functions/init";
+import dropdownMenu from "../components/dropdown-menu";
 
 initializeComponent("category-navigation", initCategories);
 
 function initCategories(rootElement) {
-    rootElement = {};
+    const scriptElement = rootElement.querySelector('script[data-items]');
+    const items = scriptElement === null ? [] : JSON.parse(scriptElement.innerHTML).items;
+    new Vue({
+        el: '#' + rootElement.id,
+        data: {
+            items: items,
+            numberOfItemsInBasket: 5
+        },
+        components: {
+            dropdownMenu
+        }
+    });
 }
-
-
-
-
