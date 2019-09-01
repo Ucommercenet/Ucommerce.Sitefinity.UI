082183574f58c3013e68f682e2de2bcbbc24b461
Tsvetan-Razsolkov <tsvetan.razsolkov@athracian.com>
2019-09-01 10:29:58 +0300

    Migrated unused products Angular JS functionality to the products.js widget Vue component.

--- Ucommerce.Sitefinity.UI/assets/src/js/widgets/products.js
+++ Ucommerce.Sitefinity.UI/assets/src/js/widgets/products.js
@@ -8,6 +8,48 @@ function initProducts(rootElement) {
         el: '#' + rootElement.id,
         components: {
             basket
+        },
+        props: {
+            productGuid: '',
+            variantGuid: '',
+            price: 0,
+            listPrice: 0
+        },
+        methods: {
+            variantUpdated: function () {
+                var variantSelector = document.getElementById('variantSku');
+                this.variantGuid = variantSelector.options[variantSelector.selectedIndex].dataset.variantGuid;
+
+                this.getPrices();
+            },
+            getPrices: function () {
+                this.$http.post('/ProductApi/productPrices',
+                    {
+                        ProductGuid: this.productGuid,
+                        VariantGuid: this.variantGuid
+                    }).then(function (response) {
+                        if (response.data) {
+                            for (var i = 0; i < response.data.length; i++) {
+                                var pricePoint = response.data[i];
+
+                                if (this.variantGuid && this.variantGuid === pricePoint.ProductGuid) {
+                                    this.listPrice = pricePoint.ListPrice;
+                                    this.price = pricePoint.Price;
+                                    break;
+                                }
+
+                                if (this.variantGuid && this.productGuid === pricePoint.ProductGuid) {
+                                    this.listPrice = pricePoint.ListPrice;
+                                    this.price = pricePoint.Price;
+                                }
+                            }
+
+                            var variantSelector = document.getElementById('variantSku');
+                            if (variantSelector.options[0].text === "")
+                                variantSelector.remove(0);
+                        }
+                    });
+            }
         }
     });
 }
