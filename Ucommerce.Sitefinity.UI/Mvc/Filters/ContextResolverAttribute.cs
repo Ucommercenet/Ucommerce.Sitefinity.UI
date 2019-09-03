0a0fb9b814f98f9d1b9d9fca5ccc7993daa9a3ab
petarmarinov <peter.marinov@athracian.com>
2019-09-03 16:03:31 +0300

    Refacgtoring Ucommerce logic to use correct FacetQuery method; Removing unused using statements from ContextResolverAttribute

--- Ucommerce.Sitefinity.UI/Mvc/Filters/ContextResolverAttribute.cs
+++ Ucommerce.Sitefinity.UI/Mvc/Filters/ContextResolverAttribute.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
-using Telerik.Sitefinity.Frontend.Mvc.Helpers;
-using Telerik.Sitefinity.Modules.GenericContent;
 using Telerik.Sitefinity.Services;
 using Telerik.Sitefinity.Web;
-using Telerik.Sitefinity.Web.DataResolving;
 using UCommerce.Runtime;
 
 namespace Ucommerce.Sitefinity.UI.Mvc.Filters
