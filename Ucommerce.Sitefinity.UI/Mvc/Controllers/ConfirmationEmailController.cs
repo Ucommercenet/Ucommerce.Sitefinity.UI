7e171b8acf4bc0082bc092a028081c7198acec0d
a.d.sotirov <a.d.sotirov@gmail.com>
2019-10-04 15:01:34 +0300

    Initial

---
+++ Ucommerce.Sitefinity.UI/Mvc/Controllers/ConfirmationEmailController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Ucommerce.Sitefinity.UI.Mvc.Model;
+
+namespace Ucommerce.Sitefinity.UI.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "uConfirmationEmail_MVC", Title = "Confirmation Email", SectionName = UcommerceUIModule.UCOMMERCE_WIDGET_SECTION, ModuleName = UcommerceUIModule.NAME, CssClass = "sfMvcIcn")]
+    public class ConfirmationEmailController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = new ConfirmationEmailController();
+            var model = ResolveModel();
+            var confirmationEmailVM = model.GetViewModel(controller);
+
+            return View("Index", confirmationEmailVM);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            base.ActionInvoker.InvokeAction(this.ControllerContext, "Index");
+        }
+
+        public ConfirmationEmailModel ResolveModel()
+        {
+            var model = new ConfirmationEmailModel();
+
+            return model;
+        }
+    }
+}
