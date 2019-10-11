6eaa77e1d62ebe9504a71ea79df17ec4d9b9b1f8
a.d.sotirov <a.d.sotirov@gmail.com>
2019-10-11 21:21:07 +0300

    Default payment option selected and added jsPaymentPicker for reference

---
+++ Ucommerce.Sitefinity.UI/Mvc/scripts/jsPaymentPicker.js
@@ -0,0 +1,38 @@
+﻿define('jsPaymentPicker', ["jquery", "jsConfig"], function ($, config) {
+	"use strict";
+
+	// declared with `var`, must be "private"
+	var classSelector = ".js-payment-picker";
+
+	function tiggerPaymentMethodChanged(paymentMethodId) {
+		config.$triggerEventSelector.trigger("payment-method-changed", {
+			paymentMethodId: paymentMethodId
+		});
+	}
+
+	function initCompleted() {
+		var paymentMethodId = config.$rootSelector.find(classSelector + ":checked").val();
+		tiggerPaymentMethodChanged(paymentMethodId);
+	}
+
+	/** START OF PUBLIC API **/
+
+	var jsPaymentPicker = {};
+
+	jsPaymentPicker.init = function () {
+		config.$rootSelector.find(classSelector).each(function() {
+			config.$triggerEventSelector.on("init-completed", { $element: $(this) }, initCompleted);
+		});
+		if (config.$rootSelector.find(classSelector + ":checked").length == 0) {
+			config.$rootSelector.find(classSelector).first().prop('checked', true);
+		};
+		config.$rootSelector.find(classSelector).on("change", (function () {
+				var paymentMethodId = $(this).val();
+				tiggerPaymentMethodChanged(paymentMethodId);
+			}));
+	};
+
+	/** END OF PUBLIC API **/
+
+	return jsPaymentPicker;
+});
