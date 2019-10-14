d5bde5a565cbf9aec56b1ef9cd3b6436043f8d76
Tihomir-Petrov <tihomir.petrov@athracian.com>
2019-10-14 19:18:07 +0200

    Ability to configure product url

--- Ucommerce.Sitefinity.UI/Mvc/Model/CartModel.cs
+++ Ucommerce.Sitefinity.UI/Mvc/Model/CartModel.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.Web;
+using Telerik.Sitefinity.Web.DataResolving;
 using Ucommerce.Sitefinity.UI.Mvc.Model.Interfaces;
 using Ucommerce.Sitefinity.UI.Mvc.ViewModels;
 using UCommerce;
@@ -45,7 +50,7 @@ namespace Ucommerce.Sitefinity.UI.Mvc.Model
                     Discount = orderLine.Discount,
                     Tax = new Money(orderLine.VAT, basket.BillingCurrency).ToString(),
                     Price = new Money(orderLine.Price, basket.BillingCurrency).ToString(),
-                    ProductUrl = CatalogLibrary.GetNiceUrlForProduct(CatalogLibrary.GetProduct(orderLine.Sku)),
+                    ProductUrl = GetProductUrl(CatalogLibrary.GetProduct(orderLine.Sku), this.productDetailsPageId),
                     PriceWithDiscount = new Money(orderLine.Price - orderLine.UnitDiscount.GetValueOrDefault(), basket.BillingCurrency).ToString(),
                     OrderLineId = orderLine.OrderLineId
                 };
@@ -62,6 +67,43 @@ namespace Ucommerce.Sitefinity.UI.Mvc.Model
             basketVM.RemoveOrderlineUrl = removeOrderLineUrl;
 
             return basketVM;
+        }
+
+        public string GetProductUrl(Product product, Guid detailPageId)
+        {
+            if (detailPageId == Guid.Empty)
+            {
+                return CatalogLibrary.GetNiceUrlForProduct(product);
+            }
+
+            var baseUrl = Ucommerce.Sitefinity.UI.Pages.UrlResolver.GetPageNodeUrl(detailPageId);
+            
+            string catUrl;
+            var productCategory = product.GetCategories().FirstOrDefault();
+            if (productCategory == null)
+            {
+                catUrl = CategoryModel.DefaultCategoryName;
+            }
+            else
+            {
+                catUrl = CategoryModel.GetCategoryPath(productCategory);
+            }
+
+            var rawtUrl = string.Format("{0}/{1}", catUrl, product.ProductId);
+            string relativeUrl = string.Concat(VirtualPathUtility.RemoveTrailingSlash(baseUrl), "/", rawtUrl);
+
+            string url;
+
+            if (SystemManager.CurrentHttpContext.Request.Url != null)
+            {
+                url = UrlPath.ResolveUrl(relativeUrl, true);
+            }
+            else
+            {
+                url = Ucommerce.Sitefinity.UI.Pages.UrlResolver.GetAbsoluteUrl(relativeUrl);
+            }
+
+            return url;
         }
 
         public CartUpdateBasketViewModel Update(CartUpdateBasket model)
