535efd55c11c2b994c3090d474fa2f8f147ca6d4
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2020-02-13 17:11:34 +0200

    Migrated mini basket to Vue.js

---
+++ UCommerce.Sitefinity.UI/assets/src/js/widgets/mini-basket.js
@@ -0,0 +1,46 @@
+﻿import { initializeComponent } from "../functions/init";
+import store from '../store';
+
+import { mapState } from 'vuex';
+
+initializeComponent("mini-basket", initCart);;
+
+function initCart(rootElement) {
+    new Vue({
+        el: '#' + rootElement.id,
+        store,
+        data: {
+            model: null
+        },
+        computed: {
+            ...mapState([
+                'updateIteration'
+            ]),
+        },
+        watch: {
+            updateIteration: function () {
+                this.fetchData();
+            }
+        },
+        methods: {
+            fetchData: function () {
+                this.$http.get(location.href + '/uc/checkout/mini-basket', {}).then((response) => {
+                    if (response.data &&
+                        response.data.status &&
+                        response.data.status == 'success' &&
+                        response.data.data && response.data.data.data) {
+
+                        this.model = response.data.data.data;
+                    }
+                    else {
+                        this.model = null;
+                    }
+                });
+            }
+        },
+        created: function () {
+            this.fetchData();
+        }
+    });
+}
+
