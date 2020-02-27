8e009a22065b202b64ad701d2d1acfa14bdc155d
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2020-02-27 12:27:28 +0200

    Product reviews, client side stage

---
+++ Ucommerce.Sitefinity.UI/assets/src/js/widgets/review-list.js
@@ -0,0 +1,37 @@
+﻿import { initializeComponent } from "../functions/init";
+
+initializeComponent("review-list", initReviewList);
+
+function initReviewList(rootElement) {
+    new Vue({
+        el: '#' + rootElement.id,
+        data: {
+            Reviews: null
+        },
+        methods: {
+            fetchData: function () {
+                this.$http.get(location.href + '/review', {}).then((response) => {
+                    if (response.data &&
+                        response.data.Status &&
+                        response.data.Status == 'success' &&
+                        response.data.Data &&
+                        response.data.Data.data && 
+                        response.data.Data.data.Reviews) {
+
+                        this.Reviews = response.data.Data.data.Reviews;
+                    }
+                    else {
+                        this.Reviews = null;
+                    }
+                });
+            }
+        },
+        created: function () {
+            this.fetchData();
+        }
+    });
+}
+
+
+
+
