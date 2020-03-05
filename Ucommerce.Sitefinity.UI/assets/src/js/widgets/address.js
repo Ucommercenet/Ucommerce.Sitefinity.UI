8b2e86b2c08250d689b61e29fc6f66e9e52e8e22
Andrey Sotirov <andrey.sotirov@americaneagle.com>
2020-03-05 12:00:10 +0200

    Add validation if data errors array is defined and has lenght Fix for average rating to not shown when no ratings are available

--- Ucommerce.Sitefinity.UI/assets/src/js/widgets/address.js
+++ Ucommerce.Sitefinity.UI/assets/src/js/widgets/address.js
@@ -20,12 +20,14 @@ function initCart(rootElement) {
             ])
         },
         watch: {
-            triggerSubmit: function () {
-                this.submit(null, false, (success) => {
-                    if (success) {
-                        this.$store.dispatch('widgetSubmitted');
-                    }
-                });
+            triggerSubmit: function() {
+                this.submit(null,
+                    false,
+                    (success) => {
+                        if (success) {
+                            this.$store.dispatch('widgetSubmitted');
+                        }
+                    });
             }
         },
         components: {
@@ -33,20 +35,19 @@ function initCart(rootElement) {
             inputField
         },
         methods: {
-            submit: function (fieldName, doNotHighlight, callback) {
+            submit: function(fieldName, doNotHighlight, callback) {
                 var fields = this.$el.querySelectorAll('input[name], select[name]');
                 var requestData = {};
                 var store = this.$store;
 
                 if (!callback) {
-                    callback = function () { };
+                    callback = function() {};
                 }
 
                 for (var field of fields) {
                     if (field.type == 'checkbox' || field.type == 'radio') {
                         requestData[field.name] = field.checked;
-                    }
-                    else {
+                    } else {
                         requestData[field.name] = field.value;
                     }
                 }
@@ -59,35 +60,36 @@ function initCart(rootElement) {
 
                         if (data.Status) {
                             if (fieldName) {
-                                if (data.Data.errors && data.Data.errors[fieldName].length) {
-                                   callback(false, data.Data.errors[fieldName]);
+                                if (typeof data.Data.errors[fieldName] !== 'undefined' && data.Data.errors[fieldName].length > 0)
+                                {
+                                    if (data.Data.errors && data.Data.errors[fieldName].length) {
+                                        callback(false, data.Data.errors[fieldName]);
+                                    }
                                 }
-                                else {
+                                else
+                                {
                                     callback(true, '');
                                 }
-                            }
-                            else if (data.Status == 'success') {
+                            } else if (data.Status == 'success') {
                                 callback(true, '');
-                            }
-                            else {
+                            } else {
                                 if (!doNotHighlight) {
                                     this.highlightFields(data.Data.errors);
                                 }
 
                                 callback(false, data.Message);
                             }
-                        }
-                        else {
+                        } else {
                             console.log("Unhandled exception");
                             callback(false, '');
                         }
                     }
                 });
             },
-            continueFn: function (callback) {
+            continueFn: function(callback) {
                 this.submit(null, false, callback);
             },
-            highlightFields: function (errors) {
+            highlightFields: function(errors) {
                 if (!errors || errors.length == 0) {
                     return;
                 }
@@ -100,28 +102,28 @@ function initCart(rootElement) {
                     }
                 }
             },
-            handleIsShippingAddressDifferent: function () {
+            handleIsShippingAddressDifferent: function() {
                 setTimeout(() => {
-                    this.submit(null, true);
-                }, 500);
+                        this.submit(null, true);
+                    },
+                    500);
             }
         },
-        created: function () {
+        created: function() {
             this.$store.commit('vuecreated', 'address');
 
             this.$http.get(location.href + '/uc/checkout/address', {}).then((response) => {
                 if (response.data &&
                     response.data.Status &&
                     response.data.Status == 'success' &&
-                    response.data.Data && response.data.Data.data) {
+                    response.data.Data &&
+                    response.data.Data.data) {
 
                     this.model = response.data.Data.data;
-                }
-                else {
+                } else {
                     this.model = null;
                 }
             });
         }
     });
 }
-
