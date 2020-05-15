2ffa665957bb0769368e1112008ede01c72c8354
Tim Gaunt <tim@thesitedoctor.co.uk>
2020-05-15 06:26:06 +0100

    [Breaking] Renamed Address to AddressViewModel for consistency
    Renamed BillingAddressDTO property to BillingAddress
    Renamed ShipmentAddressDTO property to ShipmentAddress

---
+++ Ucommerce.Sitefinity.UI/Mvc/ViewModels/AddressViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UCommerce.Sitefinity.UI.Mvc.ViewModels
+{
+	/// <summary>
+	/// DTO class that contains information related to single address.
+	/// </summary>
+	public class AddressViewModel
+	{
+		public AddressViewModel()
+		{
+			Country = new CountryViewModel();
+		}
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public string PhoneNumber { get; set; }
+		public string MobilePhoneNumber { get; set; }
+		public string Line1 { get; set; }
+		public string Line2 { get; set; }
+		public string PostalCode { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Attention { get; set; }
+		public string CompanyName { get; set; }
+		public string CountryName
+		{
+			get => Country.Name;
+			set => Country.Name = value;
+		}
+		public CountryViewModel Country { get; set; }
+	}
+}
