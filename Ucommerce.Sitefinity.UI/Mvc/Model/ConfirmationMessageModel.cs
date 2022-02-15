012c9238ebc2146c27f413d3510bd5a5afb9a2bc
Tim Gaunt <tim@thesitedoctor.co.uk>
2022-02-15 06:50:28 +0000

    UQ-224 7. Missing tracking for order completed

--- Ucommerce.Sitefinity.UI/Mvc/Model/ConfirmationMessageModel.cs
+++ Ucommerce.Sitefinity.UI/Mvc/Model/ConfirmationMessageModel.cs
@@ -1,35 +1,39 @@
 ﻿using System.Collections.Generic;
-using UCommerce.Sitefinity.UI.Mvc.Model;
+using UCommerce.Sitefinity.UI.Mvc.Services;
 using UCommerce.Sitefinity.UI.Mvc.ViewModels;
 
 namespace UCommerce.Sitefinity.UI.Mvc.Model
 {
-    /// <summary>
-    /// The Model class of the Confirmation Message MVC widget.
-    /// </summary>
-    public class ConfirmationMessageModel : IConfirmationMessageModel
-    {
-        public virtual ConfirmationMessageViewModel GetViewModel(string headline, string message)
-        {
-            var viewModel = new ConfirmationMessageViewModel()
-            {
-                Headline = headline,
-                Message = message
-            };
+	/// <summary>
+	/// The Model class of the Confirmation Message MVC widget.
+	/// </summary>
+	public class ConfirmationMessageModel : IConfirmationMessageModel
+	{
+		public IInsightUcommerceService InsightUcommerce => UCommerceUIModule.Container.Resolve<IInsightUcommerceService>();
 
-            return viewModel;
-        }
+		public virtual ConfirmationMessageViewModel GetViewModel(string headline, string message)
+		{
+			var viewModel = new ConfirmationMessageViewModel()
+			{
+				Headline = headline,
+				Message = message
+			};
 
-        public virtual bool CanProcessRequest(Dictionary<string, object> parameters, out string message)
-        {
-            if (Telerik.Sitefinity.Services.SystemManager.IsDesignMode)
-            {
-                message = "The widget is in Page Edit mode.";
-                return false;
-            }
+			InsightUcommerce.SendInteraction("Checkout", "Complete order");
 
-            message = null;
-            return true;
-        }
-    }
+			return viewModel;
+		}
+
+		public virtual bool CanProcessRequest(Dictionary<string, object> parameters, out string message)
+		{
+			if (Telerik.Sitefinity.Services.SystemManager.IsDesignMode)
+			{
+				message = "The widget is in Page Edit mode.";
+				return false;
+			}
+
+			message = null;
+			return true;
+		}
+	}
 }
